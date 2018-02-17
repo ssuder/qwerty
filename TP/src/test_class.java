0a08d3259530e5dff5644fb7b91db79858684709
tomplusa <tomasplusa@gmail.com>
2018-02-17 16:19:42 +0100

    'New_InteliJ_Project'

---
+++ TP/src/test_class.java
@@ -0,0 +1,8 @@
+/**
+ * Created by student on 17.02.2018.
+ */
+public class test_class {
+    public static void main(String[] args) {
+        System.out.println("Hello from TP computer");
+    }
+}
